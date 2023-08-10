4cdc53d1e1e2475936844c8d35e5efd8d107c927
Rusnac-Ivan <rusnacivan62@gmail.com>
2023-08-10 15:25:50 +0300

    adding module

---
+++ precache-manifest.903e3d2f17a2c752b86a2ac1a864b1bb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e206a937d814fde7820e182bd536e64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "539260187b3970f7fe85",
+    "url": "/static/css/2.51198e21.chunk.css"
+  },
+  {
+    "revision": "43c152b121e4b3839085",
+    "url": "/static/css/main.603fce87.chunk.css"
+  },
+  {
+    "revision": "539260187b3970f7fe85",
+    "url": "/static/js/2.752b6d7f.chunk.js"
+  },
+  {
+    "revision": "0b31fb14eee500646560caa49543f84c",
+    "url": "/static/js/2.752b6d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de1cfc9142e90e0e65c7",
+    "url": "/static/js/3.d078be7c.chunk.js"
+  },
+  {
+    "revision": "43c152b121e4b3839085",
+    "url": "/static/js/main.b881f575.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.b881f575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d486cfcfab883ca5872",
+    "url": "/static/js/runtime~main.e376d644.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
